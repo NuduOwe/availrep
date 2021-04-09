9d203f80c3a10f0edf7e77b05976298d1bdeff0f
NuduOwe <81234436+NuduOwe@users.noreply.github.com>
2021-04-09 08:43:16 +0200

    Add files via upload

--- availabilityReport_run.py
+++ availabilityReport_run.py
@@ -1,9 +1,29 @@
 import pandas as pd
+import json
 import datetime
 
-from availreport import Availreport
+import matplotlib.pyplot as plt
+from PIL import Image
+from pdfReport import PDF
+
+# from availreport import Availreport
 
 #########
+
+def function_error(func_name):
+	print("[ ERROR ] : function " + func_name + "() ran into an error \n")
+
+# Loads a json-file
+def import_services(json_file):
+    try:
+        with open(json_file, "r") as jsonfile:
+        	data = json.load(jsonfile)
+        	print("Configuration read in successfully...\n")
+        	return data
+    except:
+        function_error(import_services.__name__)
+        print("config-file " + json-file + " could not be imported")
+        exit()
 
 # Removes character from every row of a column of a dataframe
 def remove_char_from_column(dataframe, dataframe_column, character):
@@ -14,70 +34,156 @@ def remove_char_from_column(dataframe, dataframe_column, character):
 		print("ERROR: function remove_percent() ran into an error")
 		return False
 
-#TODO: 
+# Makes all values in dataframe column floats 
 def make_dfcolumn_float(dataframe, dataframe_column):
 	try:
 		dataframe[dataframe_column] = dataframe[dataframe_column].astype(float)
 		return True
 	except:
-		print("ERROR: function remove_percent() ran into an error")
+		function_error(make_dfcolumn_float.__name__)
 		return False
 
 
-#TODO: 
-def sel_dfrow_by_substring(dataframe, dataframe_column, substring):
+# Returns a dataframe where a defined string is found in a defined dataframe column
+def select_dfrow_by_substring(dataframe, dataframe_column, substring):
 	try: 
 		sub_dataframe = dataframe[dataframe[dataframe_column].str.contains(substring)]
 		return sub_dataframe
 	except: 
-		print("ERROR: function sel_dfrow_by_substring() ran into an error")
-		exit()	
+		function_error(select_dfrow_by_substring.__name__)
+		exit()
+
+# Returns a dataframe where the dataframe columns equal the keys of json-config file
+def create_services_dframe(dataframe, dataframe_column, config_json):
+	try:
+		print("Creating report from configuration... \n")
+		
+		services_dframe = pd.DataFrame(columns=list(dataframe.columns))
+		
+		for service in config_json:
+			services_dframe = services_dframe.append(select_dfrow_by_substring(dataframe, dataframe_column, service))
+		
+		return services_dframe
+	except:	
+		function_error(create_services_dframe.__name__)
+		exit()
+		
 	
+# Makes all values of type int64 in a dataframe column to timedelta time
+def convert_int64secs_to_time(dataframe, dataframe_column):
+	try: 
+		dataframe[dataframe_column] = dataframe[dataframe_column].apply(lambda x: datetime.timedelta(seconds=x))
+		return True
+	except:
+		function_error(convert_int64secs_to_time.__name__)
+		return False
+		
+'''
+TODO:
+
+* Should be able to add, sub, multi, div
+* Column-name should be argument
+
+'''
+'''
+def calculate_columns(dataframe, result,  column1, column2):
+	print("Dataframe: : " + dataframe)
+	print("Operation: " + operation)
+	print("Result: " + result)
+	
+	dataframe[result] = dataframe.apply(lambda x: x[column1] + x[column2], axis=1)
+	
+	
+	try: 
+		if operation == 'add':
+			dataframe[result] = dataframe.apply(lambda x: x[column1] + x[column2], axis=1)
+		if operation == 'sub':
+			dataframe[result] = dataframe.apply(lambda x: x[column1] - x[column2], axis=1)
+		if operation == 'mul':
+			dataframe[result] = dataframe.apply(lambda x: x[column1] * x[column2], axis=1)
+		if operation == 'div':
+			dataframe[result] = dataframe.apply(lambda x: x[column1] / x[column2], axis=1)
+			
+	except:
+		function_error(calculate_columns.__name__)
+	'''
+
+#########
+
+print("\nStarting availability report creation...\n")
+
+
+csv_file = 'avail.csv'
+percent = '%'
+pic_directory = 'Pictures'
+
+# import configuration
+services = import_services('service_config.json')
+
+view = [' SERVICE_DESCRIPTION',
+			' TOTAL_TIME_OK',
+			' TOTAL_TIME_CRITICAL', 
+			' PERCENT_TOTAL_TIME_OK',  
+			' PERCENT_TIME_OK_SCHEDULED', 
+			]
+
+avail_pd = pd.read_csv(csv_file, sep=',', usecols=view) 
+
+for column in view:
+	if 'PERCENT' in column:
+		if False == remove_char_from_column(avail_pd, column, percent):
+			print("ERROR: Could not remove percent from column " + column)
+		if False == make_dfcolumn_float(avail_pd, column):
+			print("ERROR: Could not convert sting to float in column " + column)
+	else:
+		if 'TIME' in column:
+			if False == convert_int64secs_to_time(avail_pd, column):
+				print("ERROR: Could not convert time in column " + column)
+
+services_avail = create_services_dframe(avail_pd, ' SERVICE_DESCRIPTION', services)
+
+# calculate_columns(services_avail, 'SUMME', ' TOTAL_TIME_OK', ' TOTAL_TIME_CRITICAL')
+
+#https://stackoverflow.com/questions/32137396/how-do-i-plot-only-a-table-in-matplotlib
+fig, ax =plt.subplots(figsize=(12,4))
+ax.axis('tight')
+ax.axis('off')
+the_table = ax.table(cellText=services_avail.values,colLabels=services_avail.columns,loc='center')
+
+#TODO: pathname building "DIR" + "month" + .png
+
+image_path = (pic_directory + "\\" + "availability-report" + "_" + "april2021" + ".png")
+
+plt.savefig(image_path)
+
+pdf_body_image = Image.open(image_path, mode="r")
+
+
+## DEBUG
+
+print(services_avail)
+
+print(avail_pd.dtypes)
+
 
 
 #########
 
-print("")
-print("Starting availability report creation...")
-print("")
+# TODO: use availreport.py
+
+#avail_pd = Availreport(col_list, 'avail.csv', ',')
+
+#avail_pd.print_all()
+
+#avail_pd.create_dataframe()
+
+#########
 
 
-csv_file = 'avail.csv'
+# PDF stuff
 
-col_list = ['HOST_NAME',
-			' SERVICE_DESCRIPTION', 
-			' PERCENT_TOTAL_TIME_OK', 
-			' PERCENT_TOTAL_TIME_CRITICAL', 
-			' TIME_OK_SCHEDULED', 
-			' TIME_OK_UNSCHEDULED']
+pdf = PDF()
+pdf.print_page(image_path)
+# TODO: filename    
+pdf.output('SalesRepot.pdf', 'F')
 
-# avail_pd = pd.read_csv(csv_file, sep=',', usecols=col_list) 
-# avail_pd = pd.read_csv(csv_file, sep=',', usecols=col_list) 
-
-avail_pd = Availreport(col_list, 'avail.csv', ',')
-
-avail_pd.print_all()
-
-avail_pd.create_dataframe()
-
-
-
-'''
-for column in col_list:
-	if 'PERCENT' in column:
-		if False == remove_char_from_column(avail_pd, column, '%'):
-			print("ERROR: Could not remove percent from column " + column)
-		if False == make_dfcolumn_float(avail_pd, column):
-			print("ERROR: Could not convert sting to float in column " + column)
-'''
-
-cycletime_avail = sel_dfrow_by_substring(avail_pd, ' SERVICE_DESCRIPTION', "cycle")
-
-
-
-
-print(avail_pd)
-
-print(avail_pd.dtypes)
-
-#datetime.fromtimestamp(2677362)
--- availreport.py
+++ availreport.py
@@ -3,21 +3,23 @@ import pandas as pd
 class Availreport():
 
 	def __init__(self, column_list, data_source, delimiter):
-		self.__column_list = column_list
-		self.__data_source = data_source
-		self.__delimiter = delimiter	
+		self._column_list = column_list
+		self._data_source = data_source
+		self._delimiter = delimiter
+		self.__dataframe = []
 		
 	def print_all(self):
-		for x in self.__column_list:
+		for x in self._column_list:
 			print(x)
-		print(self.__data_source)
-		print(self.__delimiter)	
+		print(self._data_source)
+		print(self._delimiter)	
 			
 	def create_dataframe(self):
 		try: 
-			__dataframe = pd.read_csv(self.__column_list, sep=self.__delimiter, usecols=self.__data_source)
+			self.__dataframe = pd.read_csv(self._data_source, sep=self._delimiter, usecols=self._column_list)
 		except: 
 			print("ERROR: dataframe could not be read from csv-file")
+			traceback.print_exc()
 			exit()
 		
 	# Removes character from every row of a column of a dataframe
@@ -46,4 +48,5 @@ class Availreport():
 			return sub_dataframe
 		except: 
 			print("ERROR: function sel_dfrow_by_substring() ran into an error")
+			traceback.print_exc()
 			exit()	
